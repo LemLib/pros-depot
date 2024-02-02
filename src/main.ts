a2c620ea190f7b8afa9ba2505e20a58034fb559b
Andrew Curtis <80860310+meisZWFLZ@users.noreply.github.com>
2024-02-02 23:21:49 +0000

    refactor: :truck: move from populate.ts to json.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { populateDepotJsonsFromGithub } from './populate'
+import { populateDepotJsonsFromGithub } from './create'
 import { Octokit } from '@octokit/rest'
 import { pushDepotJsonToGithub } from './pushDepot'
 import { createCommitMessage } from './message'
